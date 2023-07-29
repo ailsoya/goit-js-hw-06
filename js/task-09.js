4cd343fc3dd2e03021afb283e754e12b17199953
ailsoya <akalitata53@gmail.com>
2023-07-29 19:32:16 +0200

    8 and 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanColor = document.querySelector(".color");
+const colorChange = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+colorChange.addEventListener("click", () => {
+  spanColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+});
